be2f07aee2c6ea5b8d105d55b519eb2ece0c74be
favorable-device-5158 <blee2125@gmail.com>
2019-08-15 20:10:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(array)
   if katz_deli.length ==0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array.first}"
+    puts "Currently serving #{array.first}."
     array.shift
   end
 end
