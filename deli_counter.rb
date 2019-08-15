a416586fb756859cab3a3ad526d936ed418b81e7
favorable-device-5158 <blee2125@gmail.com>
2019-08-15 20:03:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ def line(array)
   if array.length ==0
     puts "The line is currently empty."
   else
+    current_line = "The line is currently:"
     array.each_with_index do |name, index|
-      puts "The line is currently:" <<" #{index+1}. #{name}"
+      current_line<<" #{index+1}. #{name}"
     end
   end
 end
