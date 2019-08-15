989b62daf4ba14b34c3c64a0fb67b1d4378651cc
favorable-device-5158 <blee2125@gmail.com>
2019-08-15 19:28:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 # Write your code here.
 katz_deli=[]
-def line(katz_deli)
-  if katz_deli.length ==0
+def line(array)
+  if array.length ==0
     puts "The line is currently empty."
   else
     puts "The line is currently:"
   end
 end
 
-def take_a_number(katz_deli, customer)
+def take_a_number(array, customer)
 
 end
 
