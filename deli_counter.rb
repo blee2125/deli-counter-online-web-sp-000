bfb5b6bddf035b8f16e7aba65974df7c7205e138
favorable-device-5158 <blee2125@gmail.com>
2019-08-15 19:43:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@ katz_deli=[]
 def line(array)
   if array.length ==0
     puts "The line is currently empty."
-  else array.each_with_index { |name, index| puts "The line is currently: #{index+1}. #{name}" }
+  else array.each_with_index do |name, index|
+    puts "The line is currently: #{index+1}. #{name}"
   end
 end
 
