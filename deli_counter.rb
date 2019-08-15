1c60bcdfa5d2df23fb1bb3f3b03626c0d8d3c017
favorable-device-5158 <blee2125@gmail.com>
2019-08-15 19:57:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     puts "The line is currently empty."
   else
     array.each_with_index do |name, index|
-      puts "The line is currently:"
+      print "The line is currently:"
       print" #{index+1}. #{name}"
     end
   end
