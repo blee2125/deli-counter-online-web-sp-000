3e3a6d4c0ebeca3268be5ba90813d9edb3504f15
favorable-device-5158 <blee2125@gmail.com>
2019-08-15 19:23:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+katz_deli=[]
+def line(katz_deli)
+  if katz_deli.length ==0
+    puts "The line is currently empty."
+
+  end
+end
+
+def take_a_number(katz_deli, customer)
+
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length ==0
+    puts "There is nobody waiting to be served!"
+
+  end
+
+end
